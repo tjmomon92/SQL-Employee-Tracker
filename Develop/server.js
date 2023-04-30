980e775cbb4e8b410bf9bbcd690ddde10404ff78
TJ Momon <tjmomon92@gmail.com>
2023-04-30 12:39:53 -0500

    created initialization function for program

--- Develop/server.js
+++ Develop/server.js
@@ -14,4 +14,37 @@ const db = mysql.createConnection(
     console.log(`Connected to the company_db database.`)
 );
 
-console.log(db);
+// Function to start program/return to Main
+function init() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          message: 'What would you like to do?',
+          name: 'menu',
+          choices: ['See list of departments', 'See list of employees', 'See list of roles', 'Add a department', 'Add an employee', 'Add a role', 'Update employee role', 'Quit']
+        }
+      ])
+      .then((response) => {
+        if (response.menu == "See list of departments") {
+          seeDepartments();
+        } else if (response.menu == "See list of employees") {
+          seeEmployees();
+        } else if (response.menu == "See list of roles") {
+          seeRoles();
+        } else if (response.menu == "Add a department") {
+          addDepartment();
+        } else if (response.menu == "Add a role") {
+          addRole();
+        } else if (response.menu == "Add an employee") {
+          addEmployee();
+        } else if (response.menu == "Update employee role") {
+          updateEmployeeRole();
+        } else if (response.menu == "Quit") {
+          process.exit(0)
+        }
+      })
+  };
+
+//   Call to start program
+init();
